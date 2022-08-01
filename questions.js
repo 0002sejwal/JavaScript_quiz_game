cf8906341cb0aba8460b32b79c56f9cc1aadbda7
0002sejwal <74792872+0002sejwal@users.noreply.github.com>
2022-08-01 22:18:13 +0530

    Test project for Internship
    Tasks to qualify for an internship at Dyneton:
    A webpage based JavaScript quiz game.

---
+++ questions.js
@@ -0,0 +1,69 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "How many companies are there in BSE Sensex?",
+    answer: "30",
+    options: [
+      "25",
+      "30",
+      "50",
+      "75"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Volkswagen is a company based in?\n",
+    answer: "Germany",
+    options: [
+      "Germany",
+      "Sweden",
+      "France",
+      "U.S.A"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Vitara Brezza is a car model launched by?\n",
+    answer: "Maruti",
+    options: [
+      "Hyundai",
+      "Ford",
+      "Maruti",
+      "Honda"
+    ]
+  },
+    {
+    numb: 4,
+    question: "With which company is Sundar Pichai associated?\n",
+    answer: "Google",
+    options: [
+      "Yahoo",
+      "Microsoft",
+      "Google",
+      "Amazon"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Nike is a multinational company with its headquarters in?",
+    answer: "U.S.A.",
+    options: [
+      "Sweden",
+      "France",
+      "Germany",
+      "U.S.A."
+    ]
+  },
+    {
+    numb: 6,
+    question: "The headquarters of Amazon company are located in?\n",
+    answer: "Seattle",
+    options: [
+      "Washington",
+      "California",
+      "New York",
+      "Seattle"
+    ]
+  },
+];
